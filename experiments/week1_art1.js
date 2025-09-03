4912a58255fd6b6a0f39600483fe2ce14688010c
filippahans <hanssonfilippa@hotmail.com>
2025-09-03 23:14:37 +0200

    initial commit 1

---
+++ experiments/week1_art1.js
@@ -0,0 +1,49 @@
+const size = 80;
+const gap = 20;
+const amount = 5;
+
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+  noLoop();
+}
+
+function draw() {
+  background(255);
+  let y = (height - size * amount - gap * (amount - 1)) / 2;
+  let counter = 0;
+
+  for (let i = 0; i < amount; i++) {
+    let x = (width - size * amount - gap * (amount - 1)) / 2;
+
+    for (let j = 0; j < amount; j++) {
+      push();
+      translate(x, y);
+
+      // Här är det som du kan leka med: former och färg
+      let r = random(200, 255);
+      let g = random(100, 220);
+      let b = random(100, 180);
+      fill(r, g, b, 150);
+      noStroke();
+
+      // Exempel: fyra korsande ellipser istället för square
+      let petals = 5;
+      for (let i = 0; i < petals; i++) {
+        ellipse(0, 0, size, size / 2);
+        rotate((PI * 2) / petals);
+      }
+
+      // Extra små rektanglar i mitten
+      fill(255, 255, 255, 100);
+      for (let k = 0; k < 4; k++) {
+        ellipse(-size / 8, -size / 8, size / 4, size / 4);
+        rotate(PI / 2);
+      }
+
+      pop();
+      x += size + gap;
+      counter++;
+    }
+    y += size + gap;
+  }
+}
