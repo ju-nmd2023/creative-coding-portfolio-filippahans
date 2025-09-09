5d9b55b4676423c126cb05d0dbf0142486b6ee93
filippahans <hanssonfilippa@hotmail.com>
2025-09-09 14:02:55 +0200

    initial commit

---
+++ experiments/week1_art4.js
@@ -0,0 +1,53 @@
+const size = 100;
+const amount = 24;
+const gap = 20;
+
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+  rectMode(CENTER);
+  noLoop();
+}
+
+function getRandomValue(pos, variance) {
+  return pos + random(-variance, variance);
+}
+
+function drawSquare(x, y, s) {
+  const variance = s / 2;
+  beginShape();
+  vertex(
+    getRandomValue(x - s / 2, variance),
+    getRandomValue(y - s / 2, variance)
+  );
+  vertex(
+    getRandomValue(x + s / 2, variance),
+    getRandomValue(y - s / 2, variance)
+  );
+  vertex(
+    getRandomValue(x + s / 2, variance),
+    getRandomValue(y + s / 2, variance)
+  );
+  vertex(
+    getRandomValue(x - s / 2, variance),
+    getRandomValue(y + s / 2, variance)
+  );
+  endShape(CLOSE);
+}
+
+function draw() {
+  background(255, 182, 193);
+  stroke(219, 21, 20);
+  strokeWeight(1);
+  noFill();
+
+  let y = (height - size * amount - gap * (amount - 1)) / 2;
+  for (let i = 0; i < amount; i++) {
+    let x = (width - size * amount - gap * (amount - 1)) / 2;
+
+    for (let j = 0; j < amount; j++) {
+      drawSquare(x, y, size);
+      x += size + gap;
+    }
+    y += size + gap;
+  }
+}
